b07fcee64e9cd7965da60893804d4c71de50391e
Neebe3289 <neebexd@gmail.com>
2024-06-18 23:37:48 +0000

    Fix typo
    Change-Id: I261d048044aab930bbef1560aeba54f0e589f4cb
    Signed-off-by: Neebe3289 <neebexd@gmail.com>

--- kernel/kernel_compat.c
+++ kernel/kernel_compat.c
@@ -10,7 +10,7 @@
 #include "klog.h" // IWYU pragma: keep
 #include "kernel_compat.h" // Add check Huawei Device
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI)x
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI)
 #include <linux/key.h>
 #include <linux/errno.h>
 #include <linux/cred.h>
