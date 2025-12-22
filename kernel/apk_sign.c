95bb360c29d6316f9cd05d099da907d3c103ffc9
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-12-22 08:26:45 +0000

    kernel: allow use with official manager

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -308,5 +308,7 @@ module_param_cb(ksu_debug_manager_appid, &expected_size_ops,
 
 bool is_manager_apk(char *path)
 {
-    return check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH);
+    return check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH) ||
+           check_v2_signature(path, OFFICIAL_EXPECTED_SIZE,
+                              OFFICIAL_EXPECTED_HASH);
 }
